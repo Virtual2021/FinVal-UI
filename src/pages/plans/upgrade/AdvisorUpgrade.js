1ad3cf5f635ca851395de79e62b2f2d71013b5c2
harshal144 <the harshalvijaywargiya@gmail.com>
2024-12-26 16:53:38 +0530

    update in upgrade changes

--- src/pages/plans/upgrade/AdvisorUpgrade.js
+++ src/pages/plans/upgrade/AdvisorUpgrade.js
@@ -3,9 +3,11 @@ import { formatDate, formatNumber } from "../../../common/numberUtils";
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 
+
 const AdvisorUpgrade = ({ currentPlan, data }) => {
   const navigate = useNavigate();
   const [selectedPlan, setSelectedPlan] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Set default selected plan on component mount
   useEffect(() => {
@@ -14,25 +16,116 @@ const AdvisorUpgrade = ({ currentPlan, data }) => {
   }, [data]);
 
   const handleSelection = (item) => {
-    setSelectedPlan(item);
+    setLoading(true); // Show loader
+    setTimeout(() => {
+      setSelectedPlan(item);
+      setLoading(false); // Hide loader after 1 second
+    }, 500);
   };
 
   const handleContinue = () => {
     if (selectedPlan) {
-      // Navigate to the next page with the selected plan data
       navigate("/upgrade-summary", { state: { plan: selectedPlan, currentPlan: currentPlan } });
     } else {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Please select a plan to upgrade.',
+        icon: "error",
+        title: "Error",
+        text: "Please select a plan to upgrade.",
       });
     }
   };
 
   return (
     <>
-      <div className="row">
+      {/* Mobile View */}
+      <div className="d-block d-lg-none">
+        <fieldset className="ps-15px pe-15px pt-10px mt-15px mb-15px">
+          <legend className="fw-600 float-none border-1px col-auto fs-14 ps-15px pe-15px p-5px lh-1 border-radius-4px bg-light-blue text-blue m-0 text-center">
+            Select one of the available plan options to upgrade
+          </legend>
+          <div className="table-responsive">
+              <table className="table table-striped fs-14 lh-normal mytable border-blue align-middle text-center" border="1">
+                <thead className="border-solid border-1 border-light-blue">
+                  <tr>
+                    <th scope="col" className="text-nowrap bg-blue text-white fw-600 border-solid border-1 border-blue w-70px">Select</th>
+                    <th scope="col" className="text-nowrap bg-blue text-white fw-600 border-solid border-1 border-blue">No. of<br />Reports</th>
+                    <th scope="col" className="text-nowrap bg-blue text-white fw-600 border-solid border-1 border-blue">Access<br />Days</th>
+                    <th scope="col" className="text-nowrap bg-blue text-white fw-600 border-solid border-1 border-blue">Plan Price<br />(USD)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item, index) => (
+                    <tr key={index}>
+                      <td className="fs-14 p-0 h-40px">
+                        <label className="d-block">
+                          <input
+                            className="form-check-input p-0 mt-0 text-black w-15px h-15px border-blue"
+                            type="radio"
+                            name="plan"
+                            id={`radio${index}`}
+                            value={item.id}
+                            checked={selectedPlan && selectedPlan.id === item.id}
+                            onChange={() => handleSelection(item)}
+                          />
+                          
+                        </label>
+
+                      </td>
+                      <td className="fs-14 p-0">
+                        <label htmlFor={`radio${index}`} className="d-block">{item.originalReports}</label>
+                      </td>
+                      <td className="fs-14 p-0">
+                        <label htmlFor={`radio${index}`} className="d-block">{item.originalAccessDays}</label>
+                      </td>
+                      <td className="fs-14 p-0">
+                        <label htmlFor={`radio${index}`} className="d-block">{formatNumber(item.price)}</label>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+        </fieldset>
+
+        <fieldset className="mt-3">
+          <legend className="fw-600 float-none border-1px col-auto fs-14 ps-15px pe-15px p-5px lh-1 border-radius-4px bg-light-blue text-blue m-0 text-center">
+            Your current plan gets converted to following
+          </legend>
+          <div className="table-responsive">
+            {loading ? (
+              <div className="text-center">
+                <div className="spinner-border text-primary" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            ) : (
+              <table className="table table-striped fs-14 lh-normal mytable border-blue align-middle text-center" border="1">
+                <thead>
+                  <tr>
+                    <th className="bg-blue text-white fw-600 border-solid border-1 border-blue">Total Reports</th>
+                    <th className="bg-blue text-white fw-600 border-solid border-1 border-blue">New Access Days</th>
+                    <th className="bg-blue text-white fw-600 border-solid border-1 border-blue">New Expire Date</th>
+                    <th className="bg-blue text-white fw-600 border-solid border-1 border-blue">Upgrade Price (USD)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedPlan && (
+                    <tr>
+                      <td>{selectedPlan.reports}</td>
+                      <td>{selectedPlan.access_days}</td>
+                      <td>{selectedPlan.expiresAt}</td>
+                      <td>{selectedPlan.upgrade_price}</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </fieldset>
+      </div>
+
+      {/* Desktop View */}
+      <div className="row d-none d-lg-flex">
         <div className="col-lg-6">
           <fieldset className="ps-15px pe-15px pt-10px mt-15px mb-15px md-px-0 ">
             <legend className="fw-600 float-none border-1px col-auto fs-14 ps-15px pe-15px p-5px lh-1 border-radius-4px bg-light-blue text-blue m-0 text-center">Select one of the available plan options to upgrade</legend>
@@ -104,6 +197,7 @@ const AdvisorUpgrade = ({ currentPlan, data }) => {
           </fieldset>
         </div>
       </div>
+
       <div className="row">
         <div className="col-sm-12 text-center">
           <button
